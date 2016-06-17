04f2696ad383e5d26f6d5d6bfdf22679568525b7
Julian Scharf <julzelements@gmail.com>
2016-06-17 11:00:51 +1000

    Initial Commit

---
+++ JazzHandsTests/JazzHandsTests.swift
@@ -0,0 +1,36 @@
+//
+//  JazzHandsTests.swift
+//  JazzHandsTests
+//
+//  Created by Julian Scharf on 17/06/2016.
+//  Copyright © 2016 Julian Scharf. All rights reserved.
+//
+
+import XCTest
+@testable import JazzHands
+
+class JazzHandsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
